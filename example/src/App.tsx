0566cce9da2bc4e4608e7b32e7dc0daa7d1a0e58
Maurizio Carella <maurizio.carella27@gmail.com>
2022-02-10 12:10:36 +0100

    Improved example page with tailwind3 support

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,21 +1,17 @@
-import React, {useMemo, useState} from 'react'
+import React, {useMemo, useState} from "react"
 
-import {DateTimePicker, DateTimePickerSelectorType} from 'react-datetime-pickers'
-import 'react-datetime-pickers/dist/index.css'
+import {DateTimePicker, TimePicker, DateTimePickerSelectorType} from "react-datetime-pickers"
+import "react-datetime-pickers/dist/index.css"
 
-import './App.scss'
-
-const App: React.VFC = () => {
+const Example1: React.VFC<React.HTMLProps<HTMLDivElement>> = ({...props}) => {
 	const [selected, setSelected] = useState<Date>();
-	const [selector, setSelector] = useState<DateTimePickerSelectorType>(DateTimePickerSelectorType.MONTH);
+	const [selector, setSelector] = useState<DateTimePickerSelectorType>(DateTimePickerSelectorType.DAY);
 	const [showTimePicker, setShowTimePicker] = useState(true);
 	const [disabled, setDisabled] = useState(false);
-	const [logs, setLogs] = useState<string[]>([]);
 
 	const handleDateChange = (date?: Date) => {
-		console.debug('DatePicker', 'onChange', date);
+		console.debug("DatePicker", "onChange", date);
 		setSelected(date);
-		setLogs((logs) => [date ? `Date changed: ${date.toLocaleString()}` : `Date cleared`, ...logs])
 	};
 
 	const handleSelectorChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
@@ -42,9 +38,10 @@ const App: React.VFC = () => {
 	}, [])
 
 	return (
-		<>
-			<div>
-				<label>Selector</label>
+		<div {...props}>
+			<h1>Example 1</h1>
+			<div className="mb-2">
+				<label className="block">Selector</label>
 				<select onChange={handleSelectorChange} value={selector}>
 					<option value="day">Day</option>
 					<option value="week">Week</option>
@@ -52,23 +49,27 @@ const App: React.VFC = () => {
 					<option value="year">Year</option>
 				</select>
 			</div>
-			<div>
-				<label>Show time picker</label>
+			<div className="mb-2">
 				<input
 					type="checkbox"
+					id="checkbox-show-time-picker"
 					checked={showTimePicker}
 					onChange={handleShowTimePickerChange}
+					className="mr-1"
 				/>
+				<label htmlFor="checkbox-show-time-picker">Show time picker</label>
 			</div>
-			<div>
-				<label>Disabled</label>
+			<div className="mb-2">
 				<input
 					type="checkbox"
+					id="checkbox-disabled"
 					checked={disabled}
 					onChange={handleDisabledChange}
+					className="mr-1"
 				/>
+				<label htmlFor="checkbox-disabled">Disabled</label>
 			</div>
-			<div>
+			<div className="mb-2">
 				<DateTimePicker
 					selected={selected}
 					selector={selector}
@@ -79,18 +80,46 @@ const App: React.VFC = () => {
 					timeStep={600}
 					disabled={disabled}
 					disabledDates={{
-						url: '/data/disabled-dates.json'
+						url: "/data/disabled-dates.json"
 					}}
 				/>
 			</div>
 
 			<pre>{JSON.stringify(selected)}</pre>
-
-			<pre style={{display: 'none'}}>
-				{logs.join("\n")}
-			</pre>
-		</>
+		</div>
 	);
+}
+
+const Example2: React.VFC<React.HTMLProps<HTMLDivElement>> = ({...props}) => {
+	const [selected, setSelected] = useState<Date>();
+
+	const handleDateChange = (date?: Date) => {
+		console.debug("TimePicker", "onChange", date);
+		setSelected(date);
+	};
+
+	return (
+		<div {...props}>
+			<h1>Example 2</h1>
+			<div className="mb-2">
+				<TimePicker
+					selected={selected}
+					onChange={handleDateChange}
+				/>
+			</div>
+
+			<pre>{JSON.stringify(selected)}</pre>
+		</div>
+	)
+}
+
+const App: React.VFC = () => {
+	return (
+		<div className="flex">
+			<Example1 className="grow"/>
+			<Example2 className="grow"/>
+		</div>
+	)
 }
 
 export default App
